c7dc92f9c9be82f079ba08f396e9eac6edc66cca
Pedro Rios <erpzarca@gmail.com>
2024-02-08 12:11:31 +0100

    update shmarte

--- ejecutaUserGit.bat
+++ ejecutaUserGit.bat
@@ -2,7 +2,7 @@ echo off
 cls
 echo "Ejecutando git config --global user.name Pedro Rios"
 git config --global user.name "Pedro Rios"
-echo "Ejecutando git config --global user.mail shmarte@gmail.com"
-git config --global user.email "shmarte@gmail.com"
+echo "Ejecutando git config --global user.mail erpzarca@gmail.com"
+git config --global user.email "erpzarca@gmail.com"
 echo "Ejecutando git config --global credential.username shmarte1970"
-git config --global credential.username "shmarte1970"
+git config --global credential.username "erpzarca"
